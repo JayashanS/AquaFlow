d916cf2fc1105f6757460b5090b75ec7e80af733
Himantha-alt <jayashanhimantha@gmail.com>
2025-02-19 19:28:49 +0530

    Add project files.

---
+++ AquaFlow.DataAccess/Models/WorkerPosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaFlow.DataAccess.Models
+{
+    public class WorkerPosition
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public required string Name { get; set; }
+
+        public ICollection<Worker> Workers { get; set; } = new List<Worker>();
+    }
+}
